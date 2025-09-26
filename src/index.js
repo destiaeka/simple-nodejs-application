e4b871924ba72f2d24c74214d9493b129c6ea186
destiaeka <163701104+destiaeka@users.noreply.github.com>
2025-09-26 15:06:36 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Koneksi ke MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'db',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'testdb'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Gagal konek ke database:', err);
+  } else {
+    console.log('Terhubung ke MySQL!');
+  }
+});
+
+app.get('/', (req, res) => {
+  db.query('SELECT NOW() AS now', (err, results) => {
+    if (err) return res.send('Error query database');
+    res.send(`Waktu sekarang di DB: ${results[0].now}`);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
